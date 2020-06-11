41bc01a8beee666443d2f093258c087d36b591dc
WeichselRiver <c.weichsel@gmx.de>
2020-06-11 10:55:34 +0200

    Display on same page

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 import pandas as pd
@@ -9,25 +9,23 @@ class FileUploadForm(FlaskForm):
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'secret'
-
+tbl = "Test"
 
 @app.route("/", methods=("GET", "POST",))
-def index():
+def index(tbl = tbl):
 
     form = FileUploadForm()
 
     if request.method == 'POST':
         f = request.files['upload']
         dta_xls = pd.read_excel(f)
-        return dta_xls.to_html()
+        tbl = dta_xls.to_html()
+        return render_template("index.html", form=form, tbl = tbl)
         
     if request.method == 'GET':
-        return render_template("index.html", form=form)
+        return render_template("index.html", form=form, tbl = tbl)
 
 
-@app.route("/report")
-def report():
-    return render_template("report.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
